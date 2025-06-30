bfce17400ac647ad9aa5733e7e8183cbe0a2bf1f
alerlk0912 <arruaalejandro0@gmail.com>
2025-06-30 14:56:30 -0300

    errro corregido de app

--- app/rag.py
+++ app/rag.py
@@ -1,6 +1,6 @@
 import sys
-#__import__('pysqlite3')
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import re
 import numpy as np
